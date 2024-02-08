a09ae4f8665c56bcb38a5307fe0021291de58aa4
Edward L. <59051661+Justedlev@users.noreply.github.com>
2024-02-08 21:41:38 +0200

    -

--- src/components/login/LoginComponent.tsx
+++ src/components/login/LoginComponent.tsx
@@ -12,68 +12,65 @@ import { RootState } from "../../store/store";
 import { pathLabel } from "../../config/menu";
 
 function LoginComponent() {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const loginState: LoginState = useAppSelector((state: RootState) => state.login);
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
+	const loginState: LoginState = useAppSelector((state: RootState) => state.login);
 	const accessToken: string = useAppSelector((state: RootState) => state.login.response.token.accessToken);
-  const nickname = loginState.response.nickname;
+	const nickname = loginState.response.nickname;
 
-  const [loginInput, setLoginInput] = useState<LoginRequest>({
-    nickname: "",
-    email: "",
-    password: "",
-		// refresh: loginState.response.token.refreshToken,
-  });
+	const [loginInput, setLoginInput] = useState<LoginRequest>(new LoginRequest());
 
 	if (!isEmpty(accessToken) && !isEmpty(nickname)) {
 		return <Navigate to={`${pathLabel.account.path}/${nickname}`} />;
 	}
 
-  return (
-    <>
-      {!isEmpty(loginState.error) && <MessageComponent isOpen message={loginState.error} type="error" />}
-      <Card sx={{ maxWidth: 345, m: "50px auto", boxShadow: 8, borderRadius: 4 }}>
-        <Avatar
-          alt="account"
-          variant="rounded"
-          src={accountImage}
-          sx={{ border: 0, width: 100, height: 100, m: "15px auto" }}
-        />
-        <CardContent>
-          <TextField
-            style={{ minWidth: "85%", display: "flex", margin: "auto auto 15px" }}
-            id="email"
-            label="email"
-            value={loginInput.email}
-            onChange={(e) => setLoginInput({ ...loginInput, email: e.target.value })}
-          />
-          <TextField
-            style={{ minWidth: "85%", display: "flex", margin: "auto" }}
-            id="password"
-            label="password"
-            type="password"
-            value={loginInput.password}
-            onChange={(e) => setLoginInput({ ...loginInput, password: e.target.value })}
-          />
-        </CardContent>
-        <CardActions>
-          <LoadingButton
-            sx={{ borderRadius: 50 }}
-            style={{ minWidth: "30%", margin: "auto" }}
-            variant="contained"
-            loading={loginState.isLoading}
-            onClick={() => dispatch(login(loginInput))}
-          >
-            login
-          </LoadingButton>
-        </CardActions>
-        <Typography component="p" align="center" sx={{ m: 2 }}>
-          {"No have account? "}
-          <Link onClick={() => navigate(pathLabel.signup.path)}>{pathLabel.signup.label}</Link>
-        </Typography>
-      </Card>
-    </>
-  );
+	return (
+		<>
+			{!isEmpty(loginState.error) && (
+				<MessageComponent isOpen message={loginState.error} type="error" />
+			)}
+			<Card sx={{ maxWidth: 345, m: "50px auto", boxShadow: 8, borderRadius: 4 }}>
+				<Avatar
+					alt="account"
+					variant="rounded"
+					src={accountImage}
+					sx={{ border: 0, width: 100, height: 100, m: "15px auto" }}
+				/>
+				<CardContent>
+					<TextField
+						style={{ minWidth: "85%", display: "flex", margin: "auto auto 15px" }}
+						id="email"
+						label="email"
+						value={loginInput.getEmail()}
+						onChange={(e) => setLoginInput(loginInput.setEmail(e.target.value))}
+					/>
+					<TextField
+						style={{ minWidth: "85%", display: "flex", margin: "auto" }}
+						id="password"
+						label="password"
+						type="password"
+						value={loginInput.getPassword()}
+						onChange={(e) => setLoginInput(loginInput.setPassword(e.target.value))}
+					/>
+				</CardContent>
+				<CardActions>
+					<LoadingButton
+						sx={{ borderRadius: 50 }}
+						style={{ minWidth: "30%", margin: "auto" }}
+						variant="contained"
+						loading={loginState.isLoading}
+						onClick={() => dispatch(login(loginInput))}
+					>
+						login
+					</LoadingButton>
+				</CardActions>
+				<Typography component="p" align="center" sx={{ m: 2 }}>
+					{"No have account? "}
+					<Link onClick={() => navigate(pathLabel.signup.path)}>{pathLabel.signup.label}</Link>
+				</Typography>
+			</Card>
+		</>
+	);
 }
 
 export default LoginComponent;
--- src/models/request/LoginRequest.ts
+++ src/models/request/LoginRequest.ts
@@ -1,7 +1,34 @@
-interface LoginRequest {
-  nickname: string;
-  email: string;
-  password: string;
-}
+export default class LoginRequest {
+	public constructor(
+		private nickname: string | null = null,
+		private email: string | null = null,
+		private password: string | null = null
+	) {}
 
-export default LoginRequest;
+	public getNickname(): string | null {
+		return this.nickname;
+	}
+
+	public setNickname(nicname: string): LoginRequest {
+		this.nickname = nicname;
+		return this;
+	}
+
+	public getEmail(): string | null {
+		return this.email;
+	}
+
+	public setEmail(email: string): LoginRequest {
+		this.email = email;
+		return this;
+	}
+
+	public getPassword(): string | null {
+		return this.password;
+	}
+
+	public setPassword(password: string): LoginRequest {
+		this.password = password;
+		return this;
+	}
+}
